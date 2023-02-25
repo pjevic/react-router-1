10165558a72c0d31c7f2c3af390bf4f5a7bb487f
pjevic <milanpjevic@icloud.com>
2023-02-25 17:58:30 +0100

    Navigate Programmatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate("/products");
+  };
+
   return (
     <>
       <h1>Home Page</h1>
+      <p>
+        <button onClick={navigateHandler}>Navigate to products</button>
+      </p>
     </>
   );
 };
