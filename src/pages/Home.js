1b8b31d13479707d328aafedacf891e5fc7bbc55
pjevic <milanpjevic@icloud.com>
2023-02-25 17:51:09 +0100

    Working with Navigation Links - NavLink

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,6 @@ const HomePage = () => {
   return (
     <>
       <h1>Home Page</h1>
-      <p>
-        Go to <Link to={"/products"}>the list of products</Link>.
-      </p>
     </>
   );
 };
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return <h1>Products Page</h1>;
 };
 
 export default ProductsPage;
