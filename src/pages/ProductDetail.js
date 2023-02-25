4580a3bfe881fc13c62c91930dbfd25e239dc2a8
pjevic <milanpjevic@icloud.com>
2023-02-25 18:22:01 +0100

    Adding Links for Dynamic Routes

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+
+const PRODUCTS = [
+  { id: "p1", title: "Product 1" },
+  { id: "p2", title: "Product 2" },
+  { id: "p3", title: "Product 3" },
+];
 
 const ProductDetailPage = () => {
   const params = useParams();
@@ -8,6 +14,16 @@ const ProductDetailPage = () => {
     <>
       <h1>Product Details</h1>
       <p>{params.id}</p>
+
+      <ul>
+        {PRODUCTS.map((product) => {
+          return (
+            <li key={product.id}>
+              <Link to={`/products/${product.id}`}>{product.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
