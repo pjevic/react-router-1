308acdc304b3cf640c4a49cb0715bdd6fcf8af14
pjevic <milanpjevic@icloud.com>
2023-02-25 19:01:48 +0100

    EDIT

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,29 +1,13 @@
 import React from "react";
-import { useParams, Link } from "react-router-dom";
-
-const PRODUCTS = [
-  { id: "p1", title: "Product 1" },
-  { id: "p2", title: "Product 2" },
-  { id: "p3", title: "Product 3" },
-];
+import { useParams } from "react-router-dom";
 
 const ProductDetailPage = () => {
   const params = useParams();
-
   return (
     <>
       <h1>Product Details</h1>
-      <p>{params.id}</p>
 
-      <ul>
-        {PRODUCTS.map((product) => {
-          return (
-            <li key={product.id}>
-              <Link to={`/products/${product.id}`}>{product.title}</Link>
-            </li>
-          );
-        })}
-      </ul>
+      <p>{params.id}</p>
     </>
   );
 };
