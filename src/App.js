258ef94c353c832a05b95f514828f54c9029a193
pjevic <milanpjevic@icloud.com>
2023-02-25 16:55:03 +0100

    Defining Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/products",
+    element: <ProductsPage />,
+  },
+]);
 
 const App = () => {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
